03be0d2471d4417789337b9a93a56f8a70e554e2
Krzysztof Jamrog <krzysiek.jamrog@gmail.com>
2016-04-01 20:26:21 +0200

    Algorytm Dijkstry, macierz odleglosci, centrum, centrum mini-max

--- 03.py
+++ 03.py
@@ -2,16 +2,31 @@
 
 
 from sys import argv
+from sys import maxsize
 from Graph import isGraphical
 from Graph import Graph
 from Dijkstra import dijkstra
-
+from Dijkstra import distMatrix
+from Dijkstra import centrum
+from Dijkstra import minimax
 	
 seq = [int(i) for i in argv[1:]]
 flag = isGraphical(seq)
 if flag:
 	graph = Graph(seq)
+	print("Macierz sąsiedztwa:")
 	graph.showAM()
-	temp = graph.makeEV()
-	dijkstra(graph)
+	print("\nMacierz wag poszczególnych krawędzi:")
+	for i in range(len(graph.EV)):
+		print(graph.EV[i])
+	
+
+	dists= distMatrix(graph)
+	print("\nMacierz odległości wierzchołków:")
+	for i in dists:
+		print(i)
+	print()
+
+	centrum(dists)
+	minimax(dists)
 
--- Dijkstra.py
+++ Dijkstra.py
@@ -1,21 +1,72 @@
 #!/usr/bin/env python3.4
 
-from sys import maxint
+from sys import maxsize
 from Graph import Graph
 
 # algorytm dijkstry
-# argument macierz odległości między wierzchołkami
 
 def dijkstra(graph, source):
-	
-	vset = []
-	dist = []
-	dist.append(0, None)
-	
-	for v in range(graph.vertex-1):
-		dist.append( (maxint, None) )
-		vset.appand(v+1)
+	prevs={i:None for i in range(graph.vertex)}
+	dist = {i:maxsize for i in range(graph.vertex)}
+	dist[source] = 0
+	S=[]
+	Q=[i for i in range(graph.vertex)]
+	while bool(Q):
+		u = extractMin(Q,dist)
+		S.append(u)
+		for v in graph.AL[u]:
+			if dist[v]>(dist[u]+graph.EV[u][v]):
+				dist[v] = dist[u]+graph.EV[u][v]
+				prevs[v] = u	
+	return dist
+
+def extractMin(Q, dist):
+	i=Q[0]
+	for v in Q:
+		if dist[v]<dist[i]:
+			i=v
+	Q.remove(i)
+	return i
+
+
+
+# algorytm konstruujący macierz odległości między wierzchołakami
+
+def distMatrix(graph):
+	dijkstraLists = [dijkstra(graph, i) for i in range(graph.vertex)]
+	matrix = [[dijkstraLists[i][j] for j in range(graph.vertex)] for i in range(graph.vertex)]
+
+	return matrix
 		
+
+def centrum(matrix):
+	l=sum(matrix[0])
+	m=0
+	for i in range(len(matrix)-1):
+		s=sum(matrix[i+1])
+		if s<l:
+			l=s
+			m=i+1
+	print("Centrum tego grafu to wierzchołek ", m)
+
 	
-		
-	
+def minimax(matrix):
+	l=max(matrix[0])
+	m=0
+	for i in range(len(matrix)-1):
+		s=max(matrix[i+1])
+		if s<l:
+			l=s
+			m=i+1
+	print("Centrum mini-max tego grafu to wierzchołek ", m)
+
+
+
+
+
+
+
+
+
+
+
